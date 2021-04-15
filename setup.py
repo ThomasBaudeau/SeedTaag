a4059e094ca7fdc54175d1f1979aa9a6e907e31f
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-04-15 16:27:42 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup,find_packages
+
+import SeedTaag
+
+setup(
+    name='SeedTaag',
+
+    version=SeedTaag.__version__,
+
+    packages=find_packages(),
+
+    author='TAAG',
+
+    description="A faire",
+
+    long_description="A faire",
+
+    url='https://github.com/TeamProjetM1/SeedTaag',
+
+    entry_points={
+        'console_scripts': [
+            'seedtaag = SeedTagg.__main__:main',
+        ],
+    },
+    install_requires=['networkx', 'libsbml']
+)
