0832fc36e3f040f12ca385fdd396ce2320ff9cb6
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-04-16 10:53:19 +0200

    test.1

--- test/seed_test.py
+++ test/seed_test.py
@@ -1,37 +1,21 @@
 
+import SeedTaag.topology_results as topology
+import SeedTaag.data_storage as storage
+import SeedTaag.Taagseed as Seed
+
 EXPECTED_RESULT_SEED={}
+EXPECTED_RESULT_SEED[1]={'seed':['A'],'proba':'1/1'}
+EXPECTED_RESULT_SEED[2] = {'seed': ['F','H','G'], 'proba': '1/3'}
 
-EXPECTED_RESULT_SP={}
-
-EXPECTED_RESULT_C = {}
-
-EXPECTED_RESULT_CN = {}
-
-EXPECTED_RESULT_D = {}
 
 def test_seed():
-    pass
+    S, R = storage.init_data('toy_metabolic.sbml.xml')
+    G = storage.init_graph(S, R)
+    DAG=Seed.dag_init(R, G)
+    specie = Seed.scc_species(G)
+    result = Seed.find_seed(DAG, specie)
+    assert EXPECTED_RESULT_SEED==result
 
 
-def test_SP():
-    pass
-
-
-def test_C():
-    pass
-
-
-def test_CN():
-    pass
-
-
-def test_D():
-    pass
-
-
-def test_SP():
-    pass
-
-
-def test_SP():
-    pass
+if __name__=="__main__":
+    test_seed()
