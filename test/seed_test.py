b1251098d754870ece2208f2b4ddb636175b2b2a
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-04-16 10:34:40 +0200

    test

--- test/seed_test.py
+++ test/seed_test.py
@@ -1,1 +1,37 @@
 
+EXPECTED_RESULT_SEED={}
+
+EXPECTED_RESULT_SP={}
+
+EXPECTED_RESULT_C = {}
+
+EXPECTED_RESULT_CN = {}
+
+EXPECTED_RESULT_D = {}
+
+def test_seed():
+    pass
+
+
+def test_SP():
+    pass
+
+
+def test_C():
+    pass
+
+
+def test_CN():
+    pass
+
+
+def test_D():
+    pass
+
+
+def test_SP():
+    pass
+
+
+def test_SP():
+    pass
