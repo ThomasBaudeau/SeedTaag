197c137d252653f7f103125071c6a339a3771c40
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-04-26 15:32:45 +0200

    correction et ajout

--- SeedTaag/graph_formation.py
+++ SeedTaag/graph_formation.py
@@ -1,43 +1,40 @@
 import networkx as nx
 import SeedTaag.Class as C
 
-def create_graphe():
-    """
-    built digraph object
-    """
-    G = nx.DiGraph()
-    return G
 
-
-def extract_species(Metabos, G,Mclass=True):
+def extract_species(Metabos):
     G = nx.DiGraph()
     """
     extract informations of the dictionnary and built graph node
     """
-    if Mclass:
-        for key in Metabos:
+    for key in Metabos:
             properties=Metabos[key].properties()
-            G.add_node(key, id=properties['id'],name=properties['name'],compartiment=properties['compartment'])
-        return G
-    else:
-        for key in Metabos:
-            G.add_node(key)
-        return G
-
-def extract_reactions(Reactions, G,Mclass=True):
+            G.add_node(key, id=properties['id'],name=properties['name'],compartiment=properties['compartment'],
+                       boundaryConditions=properties['boundaryConditions'], hasOnlySubtanceUnit=properties['hasOnlySubtanceUnit'],
+                       constant=['constant'])
+    return G
+   
+def extract_reactions(Reactions, G):
     """
     extract information about reaction in model libSMBL object
     """
     for key in Reactions:
-        if Mclass:
-            reactants=Reactions[key].get_reactifs()
-            products = Reactions[key].get_products()
-            reversible = Reactions[key].getReversible()
-            for reactant in reactants:
-                for product in products:
-                    G.add_edge(reactant, product, id=key)
-                    if (reversible):
-                        G.add_edge(product, reactant, id=key)
-        else:
-            G.add_edge(Reactions[key]['r'], Reactions[key]['p'], id=key)
+        properties = Reactions[key].properties()
+        for reactant in properties['reactifs']:
+            for product in properties['products']:
+                G.add_edge(reactant.get_id(), product.get_id(), id=key,name=properties['name'],
+                enzymes=properties['enzymes'])
+                if (properties['reversible']):
+                    G.add_edge(product, reactant, id=key,
+                                name=properties['name'], enzymes=properties['enzymes'])
     return G
+
+
+def dag_init(node,edge):
+    dag = nx.DiGraph()
+    for key in node:
+        dag.add_node(key, id='scc'+str(key), group=node[key]['groupe'],
+        lenght=node[key]['length'])
+    for key in edge:
+        dag.add_edge(edge[key]['r'],edge[key]['p'], id=key)
+    return dag
